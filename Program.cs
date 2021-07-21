93f5c3ebca8a5537e8f23f97853d616937d6a8e2
tatarints <87334461+tatarints@users.noreply.github.com>
2021-07-21 18:57:09 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace lesson1
         static void Main(string[] args)
         {
 
+            //Написать программу, выводящую в консоль текст: «Привет, %имя пользователя%, сегодня %дата%». Имя пользователя сохранить из консоли в промежуточную переменную.
+           
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
 
