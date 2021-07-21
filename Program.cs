a6f8ecb53df86e0d5af4cda54bc6c60164510edc
tatarints <87334461+tatarints@users.noreply.github.com>
2021-07-21 18:41:39 +0500

    lesson_1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is the date today?");
+            string date = Console.ReadLine();
+
+            Console.WriteLine($"Hi,{name}. Today: {date}");
+        }
+    }
+}
