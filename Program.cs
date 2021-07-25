ed46a3b0d5a6c133692f664be143c09d2675ee23
tatarints <87334461+tatarints@users.noreply.github.com>
2021-07-25 14:57:19 +0500

    lesson_2

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.WriteLine("What is the minimum temperature?");
+            //int min = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("What is the maximum temperature?");
+            //int max = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"Average daily temperature:{(min + max) / 2}");
+
+
+
+            //2
+            //Console.WriteLine("What month is it now?");
+            //int month = Convert.ToInt32(Console.ReadLine());
+            //DateTime now = DateTime.Now;
+            //Console.WriteLine(now.ToString("MMM"));
+            //Во втором задании, долго думала, как можно сделать програму через enum, но не нашла как запрашивать вывод данных из enum
+
+
+
+            //3
+            //Console.WriteLine("Enter the namber:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //if ((a % 2) == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not even");
+            //}
+
+
+
+            //4
+            //Console.WriteLine("Цена    Кол-во    Итого");
+            //Console.WriteLine("3276166 Семена:");
+            //Console.WriteLine("26.99   4         107.96");
+            //Console.WriteLine("3223205 Кофе:");
+            //Console.WriteLine("179.99  1         179.99");
+            //Console.WriteLine("3640958 Томат розовый 1кг:");
+            //Console.WriteLine("199.99  0.540     107.99");
+            //Console.WriteLine("Итог:             395.94");
+            //Console.WriteLine("Кассир: Рогачев И 26.12.19 13:24");
+
+
+
+            
+        }
+    }
+}
