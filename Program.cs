f18df3dcf32533d3c4dd113ba1fb5b0874c4e382
tatarints <87334461+tatarints@users.noreply.github.com>
2021-07-29 02:58:48 +0500

    lesson_3

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //int[,] arr = new int[7, 7];
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int j= 0; j < arr.GetLength(1); j++)
+            //    {
+            //        if(i == j)
+            //        {
+            //            arr[i, j] = 1;
+            //            Console.Write($"{arr[i, j]}");
+            //        }
+            //        else
+            //        {
+            //            Console.Write($"{arr[i, j]}");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            //2-Телефонный справочник
+            //string[,] telsp = new string[5, 2]{
+            //    {"Ben", "9-98-00"},
+            //    {"Sofia", "8-56-09"},
+            //    {"Den","9-34-54"},
+            //    {"Tatyana", "4-36-84"},
+            //    {"Lily", "9-09-27" }
+            //}
+
+
+
+            //3
+            //char[] chars = { 'o', 'l', 'l', 'e', 'H' };
+
+            //int c = chars.Length; // длина массива
+            //int k = c / 2;          // середина массива
+            //char lastIndex;               // вспомогательный элемент для обмена значениями
+            //for (int i = 0; i < k; i++)
+            //{
+            //    lastIndex = chars[i];
+            //    chars[i] = chars[c - i - 1];
+            //    chars[c - i - 1] = lastIndex;
+            //}
+            //foreach (int i in chars)
+            //{
+            //        Console.Write(i);
+            //}
+            //Программу написала, но смогла вывести только числовые значения, а как переходить к буквам недопонимаю.
+            //Методичку прочитала, ничего не нашла. Посмотрела в интернете, сложно всё объясняют. Помогите, пожалуйста!
+
+            //Console.WriteLine(System.Text.Encoding.Default.HeaderName);
+
+
+
+        }
+    }
+}
