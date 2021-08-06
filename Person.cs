9fc714f7ed4d0d02c8f9e830b5aba9005a45733c
tatarints <87334461+tatarints@users.noreply.github.com>
2021-08-07 03:40:09 +0500

    lesson_5

---
+++ Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson_5
+{
+    class Person
+    {
+        public static int current_year = 2021;
+        public string name { get; set; }
+        public string position { get; set; } 
+        public string email { get; set; }   
+        public string telephone { get; set; }
+        public float wage { get; set; }        
+        public int birthyear { get; set; }
+        public Person(string name,
+        string position,
+        string email,
+        string telephone,
+        float wage,
+        int birthyear)
+        {
+            this.name = name;
+            this.position = position;
+            this.email = email;
+            this.telephone = telephone;
+            this.wage = wage;
+            this.birthyear = birthyear;
+        }
+        public int GetAge()
+        {
+            return current_year - birthyear;
+        }
+        
+        public string Info()
+        {
+            return
+                this.name +", "+ this.position +", "+ this.email +", " + this.telephone +", " + this.wage +", "+ this.birthyear;
+                
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            string filename = "text.txt";
+            File.WriteAllText(filename, Console.ReadLine());
+
+
+
+            //2
+            string filedate = "startup.txt";
+            DateTime time = DateTime.Now;
+            string times = time.ToString();
+            File.WriteAllText(filedate, times);
+
+        }
+    }
+
+
+
+
+    //3
+    [Serializable]
+    class Numbers
+    {
+        public string Num { get; set; }
+        public Numbers(string num)
+        {
+            Num = num;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // объект для сериализации
+            Console.WriteLine("Введите с клавиатуры произвольный набор чисел 0...255");
+            Numbers numbers = new Numbers(Console.ReadLine());
+            Console.WriteLine("Объект создан");
+
+            // создаем объект BinaryFormatter
+            BinaryFormatter formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("numb.bin", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, numbers);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+    //4
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persArray = {
+             new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30),
+             new Person("Efremov Dima", "Doctor", "efre@gmail.com", "89920188721", 36000, 20),
+             new Person("Tatarintsev Vova", "Military officer", "tata@mailbox.com", "89785670954", 45000, 43),
+             new Person("Melkin Sasha", "Engineer", "samel@mailbox.com", "89874564327", 20000, 45),
+             new Person("Lepen Zhenya", "Driver", "lezh@mailbox.com", "89238764563", 16000, 24),
+            };
+            for (int i = 0; i < persArray.Length; i++)
+            {
+                if (persArray[i].GetAge() > 40)
+                {
+                    Console.WriteLine(persArray[i].Info());
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < persArray.Length; i++)
+            {
+                Console.WriteLine(persArray[i].Info());
+            }
+            Console.WriteLine();
+        }
+    }
+}
+    
+
+   
