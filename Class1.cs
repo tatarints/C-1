e68bfae46ff233e407d8e82b27a9d343adc323c3
tatarints <87334461+tatarints@users.noreply.github.com>
2021-08-11 18:36:20 +0500

    lesson_6

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson_6
+{
+    [Serializable]
+    class MyArrayDataException: Exception
+    {
+        int row, column;
+
+        public MyArrayDataException(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+    }
+}
