278bfb35d6890fec25b8f37460a2de70e4c31697
tatarints <87334461+tatarints@users.noreply.github.com>
2021-09-11 16:28:52 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lesson_2._2._2
+{
+    class Program
+    {
+        //метод бинарного поиска с использованием цикла
+        static int BinarySearch(int[] array, int searchedValue, int left, int right)
+        {
+            //пока не сошлись границы массива
+            while (left <= right)
+            {
+                //индекс среднего элемента
+                var middle = (left + right) / 2;
+
+                if (searchedValue == array[middle])
+                {
+                    return middle;
+                }
+                else if (searchedValue < array[middle])
+                {
+                    //сужаем рабочую зону массива с правой стороны
+                    right = middle - 1;
+                }
+                else
+                {
+                    //сужаем рабочую зону массива с левой стороны
+                    left = middle + 1;
+                }
+            }
+            //ничего не нашли
+            return -1;
+        }
+        //O(log_2(n))
+    }
+}
